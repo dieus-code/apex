23f146bd840d67d5fc753c99d0bceaefedd4bf08
Paschalis6082 <Obidiugwupaschalis@gmail.com>
2026-07-31 15:24:13 +0100

    Added authentication layout and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
 import LoginCard from "./components/LoginCard";
-import GridBackground from "./components/GridBackground";
+// import Register from "./components/Register";
+// import Verification from "./pages/Verification";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
-    <GridBackground>
-      <div className="flex min-h-screen items-center justify-center px-4">
-        <LoginCard />
-      </div>
-    </GridBackground>
+    <Routes>
+      <Route path="/" element={<LoginCard />} />
+      <Route path="/login" element={<LoginCard />} />
+
+      {/* <Route path="/register" element={<Register />} /> */}
+      {/* <Route path="/verification" element={<Verification />} /> */}
+      {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+      {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
+    </Routes>
   );
 }
 
