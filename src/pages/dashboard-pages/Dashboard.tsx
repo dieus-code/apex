3b71844017fc53c50a2bd0f89566bec6cff8ae52
ireoluwa-charis <eniolorundacharis@gmail.com>
2026-08-10 11:47:24 +0100

    update dashboard UI

--- src/pages/dashboard-pages/Dashboard.tsx
+++ src/pages/dashboard-pages/Dashboard.tsx
@@ -1,23 +1,31 @@
 import DashHeader from "../../components/dashboard-components/DashHeader";
 import DashboardGrid from "../../components/dashboard-components/DashboardGrid";
+import TransactionTable from "../../components/dashboard-components/TransactionTable";
 import avatar from "../../assets/avatar.png";
 import Layout from "../../components/dashboard-components/layout";
+
 const Dashboard = () => {
-    return (
-      <Layout>
-       <DashHeader
-         avatar={
-          <img
-            src={avatar}
-            alt="Arthur Taylor"
-            className="h-full w-full rounded-full object-cover"
-          />
-        }
-        title="Arthur Taylor"
-        subtitle="Welcome back to Apex 👋"
+  return (
+    <Layout>
+      <div className="p-6 space-y-6">
+        <DashHeader
+          avatar={
+            <img
+              src={avatar}
+              alt="Arthur Taylor"
+              className="h-full w-full rounded-full object-cover"
+            />
+          }
+          title="Arthur Taylor"
+          subtitle="Welcome back to Apex 👋"
         />
-      </Layout>
-      
-    );
-}
+
+        <DashboardGrid />
+
+        <TransactionTable />
+      </div>
+    </Layout>
+  );
+};
+
 export default Dashboard;
