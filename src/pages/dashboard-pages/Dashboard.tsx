a074db4909f7969fade762d967a7e8ebe2c4cf9d
Paschalis6082 <Obidiugwupaschalis@gmail.com>
2026-08-10 12:39:47 +0100

    resolving conflict

--- src/pages/dashboard-pages/Dashboard.tsx
+++ src/pages/dashboard-pages/Dashboard.tsx
@@ -1,31 +1,23 @@
 import DashHeader from "../../components/dashboard-components/DashHeader";
 import DashboardGrid from "../../components/dashboard-components/DashboardGrid";
-import TransactionTable from "../../components/dashboard-components/TransactionTable";
 import avatar from "../../assets/avatar.png";
 import Layout from "../../components/dashboard-components/layout";
-
 const Dashboard = () => {
-  return (
-    <Layout>
-      <div className="p-6 space-y-6">
-        <DashHeader
-          avatar={
-            <img
-              src={avatar}
-              alt="Arthur Taylor"
-              className="h-full w-full rounded-full object-cover"
-            />
-          }
-          title="Arthur Taylor"
-          subtitle="Welcome back to Apex 👋"
+    return (
+      <Layout>
+       <DashHeader
+         avatar={
+          <img
+            src={avatar}
+            alt="Arthur Taylor"
+            className="h-full w-full rounded-full object-cover"
+          />
+        }
+        title="Arthur Taylor"
+        subtitle="Welcome back to Apex 👋"
         />
-
-        <DashboardGrid />
-
-        <TransactionTable />
-      </div>
-    </Layout>
-  );
-};
-
+      </Layout>
+      
+    );
+}
 export default Dashboard;
