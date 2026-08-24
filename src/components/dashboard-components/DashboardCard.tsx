62d982fb81f53061d93795361e35b56be4da51c6
ireoluwa-charis <eniolorundacharis@gmail.com>
2026-08-24 10:35:14 +0100

    fix dashboard card layout

--- src/components/dashboard-components/DashboardCard.tsx
+++ src/components/dashboard-components/DashboardCard.tsx
@@ -11,7 +11,7 @@ function DashboardCard({
 }: DashboardCardProps) {
   return (
     <div
-      className={`rounded-xl border border-gray-200 bg-white p-5 shadow-sm ${className}`}
+      className={`rounded-xl border border-gray-200 bg-white p-5 shadow-sm w-[352px] h-[380px] ${className}`}
     >
       {children}
     </div>
